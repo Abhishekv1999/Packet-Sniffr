649ab1518584814c01933dead7264e1f101f173e
Abhishekv1999 <44601104+Abhishekv1999@users.noreply.github.com>
2019-10-01 23:11:42 +0530

    Add files via upload

---
+++ 2 d array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,n,m;
+	int a[i][j],total;
+	cout<<"enter the number of rows";
+	cin>>i;
+	cout<<"enter the number of column"; 
+	cin>>j;
+
+	cout<<"The sum is "<<total;
+	return 0;
+}
